198be9bd4227b479f6fed1b0704d787e6e302eca
patelavtx <apatel@aviatrix.com>
2023-07-17 10:32:41 +0100

    update backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
   # variables cannot be used, so hard-code
-  resource_group_name  = "atulrg-azctrlnoninit"
-  storage_account_name = "atulazctrlnoninit"
-  container_name       = "atulctr-azctrlnoninit"
+  resource_group_name  = "atulrg-ghapy"
+  storage_account_name = "atulghapy"
+  container_name       = "atulctr-ghapy"
   key       = "terraform.tfstate"
 }
 }
