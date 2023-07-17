8319d213909058d54f0347bb902df8b2d7cc34b2
patelavtx <apatel@aviatrix.com>
2023-07-17 11:59:17 +0100

    destroy

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ aviatrix_controller_azure" {
 }
 */
 
+/*
 module "aviatrix_controller_build" {
   source  = "AviatrixSystems/azure-controller/aviatrix//modules/aviatrix_controller_build"
   version = "2.0.2"
@@ -115,3 +116,4 @@ module "copilot_build_azure" {
   ]
 }
 
+*/
